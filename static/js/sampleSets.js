fb37ec3203bc347c47ded61436e08f1dbd6aa922
marisagloor <52256275+marisagloor@users.noreply.github.com>
2021-06-17 17:34:51 -0700

    update rules

--- static/js/sampleSets.js
+++ static/js/sampleSets.js
@@ -2,12 +2,12 @@ const VALIDSETS = [
     {'id': 'ps1','cards': [{'cardId': '1ReSoDi', 'color': 'Red', 'fill': 'Solid', 'numShapes': '1', 'shape': 'Diamond'}, 
     {'cardId': '2ReSoOv', 'color': 'Red', 'fill': 'Solid', 'numShapes': '2', 'shape': 'Oval'}, 
     {'cardId': '3ReSoSq', 'color': 'Red', 'fill': 'Solid', 'numShapes': '3', 'shape': 'Squiggle'}],
-    'reason': 'same color, same fill, different number, different shape' },
+    'reason': 'All three cards have same color, same fill, different number, different shape' },
     {'id': 'ps2','cards': [{'cardId': '2ReEmDi', 'color': 'Red', 'fill': 'Empty', 'numShapes': '2', 'shape': 'Diamond'}, 
     {'cardId': '2ReEmSq', 'color': 'Red', 'fill': 'Empty', 'numShapes': '2', 'shape': 'Squiggle'}, 
     {'cardId': '2ReEmOv', 'color': 'Red', 'fill': 'Empty', 'numShapes': '2', 'shape': 'Oval'}],
-    'reason':'same color same fill same number different shapes'}, 
+    'reason':'All three cards have same color, same fill, same number, different shapes'}, 
     {'id': 'ps3','cards':[{'cardId': '3GrEmSq', 'color': 'Green', 'fill': 'Empty', 'numShapes': '3', 'shape': 'Squiggle'}, 
     {'cardId': '3PuSoSq', 'color': 'Purple', 'fill': 'Solid', 'numShapes': '3', 'shape': 'Squiggle'}, 
     {'cardId': '3ReSlSq', 'color': 'Red', 'fill': 'Slated', 'numShapes': '3', 'shape': 'Squiggle'}],
-    'reason':'different color different fill same number same shape'}];
+    'reason':'All three cards have different color, different fill, same number, same shape'}];
